ae02886dc609e7a7e9ec3d06b5477116214bdd73
Yuta Okuma <y.dream.nest@icloud.com>
2021-09-03 03:10:39 +0900

    ViewComposerのリファクタリング

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    public function getMyNotes() {
+        $query_tag = \Request::query('tag');
+        // ===== メソッドのベース開始 =====
+        // タグ絞り込み
+        $query = Note::query()
+            ->select('notes.*')
+            ->where('user_id', '=', \Auth::id())
+            ->whereNull('deleted_at')
+            ->orderBy('updated_at', 'DESC');
+        // ===== メソッドのベース終了 =====
+
+        // もしクエリパラメータtagがあれば
+        if (!empty($query_tag)) {
+            $query
+                ->leftJoin('note_tags', 'note_tags.note_id', '=', 'notes.id')
+                ->where('note_tags.tag_id', '=', $query_tag);
+        }
+
+        $notes = $query->get();
+
+        return $notes;
+    }
 }
